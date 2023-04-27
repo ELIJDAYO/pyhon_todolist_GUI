77af238ba7463c4b795f9d067c14c9f014068fb5
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-04-27 12:38:23 +0800

    Added exit button

--- gui.py
+++ gui.py
@@ -12,15 +12,20 @@ list_box = sg.Listbox(values=fun.get_todos(),
                       enable_events=True,
                       size=[45,10])
 edit_button = sg.Button("Edit")
-
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 window = sg.Window("Todo App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=("Helvetica", 20))
 
 while True:
     event, values = window.read()
     print(event)
     print(values)
+    print(values["todos"])
     match event:
         case "Add":
             todos = fun.get_todos()
@@ -36,8 +41,19 @@ while True:
             todos[index] = new_todo
             fun.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = fun.get_todos()
+            todos.remove(todo_to_complete)
+            fun.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
+        case "Exit:":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
-        case sg.WIN_CLOSED: break
+            break
+        case sg.WIN_CLOSED:
+            break
 
 window.close()
